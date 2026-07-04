a73a4e49195474b1fab19df7c622a06c194eca88
Ironfort9 <82671967+Ironfort9@users.noreply.github.com>
2026-07-04 15:15:45 +0100

    Add files via upload

---
+++ mask_mesh.gd
@@ -0,0 +1,14 @@
+extends MeshInstance3D
+
+var mask_mesh : MeshInstance3D = self
+
+
+func _init() -> void:
+	if ResourceLoader.exists("res://masks/curr_mask.res"):
+		material_override = ORMMaterial3D.new()
+		material_override.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		material_override.albedo_texture = ResourceLoader.load("res://masks/curr_mask.res", "ImageTexture")
+
+
+func _ready() -> void:
+	pass
