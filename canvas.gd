2c1a4ae6a490b6cfc27f9bd7be41d613784b01e1
Ironfort9 <luiscardoso2006@gmail.com>
2026-07-04 23:27:22 +0100

    OOps

--- canvas.gd
+++ canvas.gd
@@ -1,47 +1,45 @@
 extends Node3D
 
-@export var canvas_size : Vector2i = Vector2i(1024, 1024)
-@export var draw_size : Vector2 = Vector2(10.0, 10.0)
-@export var draw_material : Material
-@export var draw_albedo_texture : Texture2D
-@export var base_mask : CompressedTexture2D
-@export_color_no_alpha var base_mask_color : Color = Color.SADDLE_BROWN
+@export var canvas_size: Vector2i = Vector2i(1024, 1024)
+@export var draw_size: Vector2 = Vector2(10.0, 10.0)
+@export var draw_material: Material
+@export var draw_albedo_texture: Texture2D
+@export var base_mask: CompressedTexture2D
+@export_color_no_alpha var base_mask_color: Color = Color.SADDLE_BROWN
 
-@onready var canvas_mesh : MeshInstance3D = $Canvas/MeshInstance3D
-@onready var camera : Camera3D = $Camera3D
+var new_mask_path: String = "res://masks/curr_mask.res"
+var base_mask_path: String = "res://masks/base_mask.res"
+var color: Color
+var albedo_texture: DrawableTexture2D
+var faces: PackedVector3Array = PackedVector3Array()
+var uvs: PackedVector2Array = PackedVector2Array()
+var triangle_mesh: TriangleMesh = TriangleMesh.new()
 
-var new_mask_path : String = "res://masks/curr_mask.res"
-var base_mask_path : String = "res://masks/base_mask.res"
+@onready var canvas_mesh: MeshInstance3D = $Canvas/MeshInstance3D
+@onready var camera: Camera3D = $Camera3D
 
-var color : Color;
-var albedo_texture : DrawableTexture2D
 
-var faces : PackedVector3Array = PackedVector3Array()
-var uvs : PackedVector2Array = PackedVector2Array()
-var triangle_mesh : TriangleMesh = TriangleMesh.new()
-
-func _ready() -> void:	
+func _ready() -> void:
 	# Setup material
-	var material : ORMMaterial3D = canvas_mesh.material_override
+	var material: ORMMaterial3D = canvas_mesh.material_override
 	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-	var drawable_texture : DrawableTexture2D = DrawableTexture2D.new()
+	var drawable_texture: DrawableTexture2D = DrawableTexture2D.new()
 	drawable_texture.setup(canvas_size.x, canvas_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8_SRGB, Color.TRANSPARENT)
 	drawable_texture.blit_rect(Rect2i(Vector2i(0, 0), canvas_size), base_mask, base_mask_color)
-	ResourceSaver.save(drawable_texture, base_mask_path)
+	ResourceSaver.save(drawable_texture.get_image(), base_mask_path)
 	material.albedo_texture = drawable_texture
 	albedo_texture = material.albedo_texture
 
-	 
 	# Setup mesh data
-	var surface : Array = canvas_mesh.mesh.surface_get_arrays(0)
-	var vertices : PackedVector3Array = surface[Mesh.ARRAY_VERTEX]
-	var tex_uvs : PackedVector2Array = surface[Mesh.ARRAY_TEX_UV]
-	var indices : PackedInt32Array = surface[Mesh.ARRAY_INDEX]
-	var index_count : int = indices.size()
+	var surface: Array = canvas_mesh.mesh.surface_get_arrays(0)
+	var vertices: PackedVector3Array = surface[Mesh.ARRAY_VERTEX]
+	var tex_uvs: PackedVector2Array = surface[Mesh.ARRAY_TEX_UV]
+	var indices: PackedInt32Array = surface[Mesh.ARRAY_INDEX]
+	var index_count: int = indices.size()
 	uvs.resize(index_count)
 	faces.resize(index_count)
-	for index in range(index_count) :
-		var vertex_idx : int = indices[index]
+	for index in range(index_count):
+		var vertex_idx: int = indices[index]
 		uvs[index] = tex_uvs[vertex_idx]
 		faces[index] = vertices[vertex_idx]
 	triangle_mesh.create_from_faces(faces)
@@ -50,51 +48,51 @@ func _ready() -> void:
 		draw_material.set_shader_parameter("texture_size", canvas_size)
 
 
-func _physics_process(_delta : float) -> void:
+func _physics_process(_delta: float) -> void:
 	pass
 
 
-func _unhandled_input(event : InputEvent) -> void :
-	if not draw_material :
+func _unhandled_input(event: InputEvent) -> void:
+	if not draw_material:
 		return
 	if not color:
 		return
-	if event is InputEventMouseMotion or event is InputEventMouseButton :
-		if event.button_mask & MOUSE_BUTTON_MASK_LEFT :
+	if event is InputEventMouseMotion or event is InputEventMouseButton:
+		if event.button_mask & MOUSE_BUTTON_MASK_LEFT:
 			var uv = calculate_uv(event)
-			if not uv.is_zero_approx() :
-				var rect : Rect2 = Rect2(uv * Vector2(canvas_size) - draw_size * 0.5, draw_size)
+			if not uv.is_zero_approx():
+				var rect: Rect2 = Rect2(uv * Vector2(canvas_size) - draw_size * 0.5, draw_size)
 				albedo_texture.blit_rect(rect, draw_albedo_texture, Color(color), 0, draw_material)
 
 
-func calculate_uv(event : InputEvent) -> Vector2:
-	var ray_origin : Vector3 = camera.project_ray_origin(event.position)
-	var ray_normal : Vector3 = camera.project_ray_normal(event.position)
-	var inv_transform : Transform3D = canvas_mesh.global_transform.inverse()
+func calculate_uv(event: InputEvent) -> Vector2:
+	var ray_origin: Vector3 = camera.project_ray_origin(event.position)
+	var ray_normal: Vector3 = camera.project_ray_normal(event.position)
+	var inv_transform: Transform3D = canvas_mesh.global_transform.inverse()
 	ray_origin = inv_transform * ray_origin
 	ray_normal = inv_transform.basis * ray_normal
-	
-	var intersect : Dictionary = triangle_mesh.intersect_ray(ray_origin, ray_normal)
+
+	var intersect: Dictionary = triangle_mesh.intersect_ray(ray_origin, ray_normal)
 	if not intersect.is_empty():
-		var index : int = intersect.face_index * 3
-		var f : Vector3 = intersect.position
-		var p1 : Vector3 = faces[index]
-		var p2 : Vector3 = faces[index + 1]
-		var p3 : Vector3 = faces[index + 2]
-		
+		var index: int = intersect.face_index * 3
+		var f: Vector3 = intersect.position
+		var p1: Vector3 = faces[index]
+		var p2: Vector3 = faces[index + 1]
+		var p3: Vector3 = faces[index + 2]
+
 		# Calculate vectors from point f to vertices p1, p2 and p3
-		var f1 : Vector3 = p1 - f
-		var f2 : Vector3 = p2 - f
-		var f3 : Vector3 = p3 - f
-		
+		var f1: Vector3 = p1 - f
+		var f2: Vector3 = p2 - f
+		var f3: Vector3 = p3 - f
+
 		# Calculate the areas and factors
-		var area : float = (p1 - p2).cross(p1 - p3).length()
-		var a1 : float = f2.cross(f3).length() / area
-		var a2 : float = f3.cross(f1).length() / area
-		var a3 : float = f1.cross(f2).length() / area
-		
+		var area: float = (p1 - p2).cross(p1 - p3).length()
+		var a1: float = f2.cross(f3).length() / area
+		var a2: float = f3.cross(f1).length() / area
+		var a3: float = f1.cross(f2).length() / area
+
 		# Find the uv corresponding to point f
-		var uv : Vector2 = uvs[index] * a1 + uvs[index + 1] * a2 + uvs[index + 2] * a3
+		var uv: Vector2 = uvs[index] * a1 + uvs[index + 1] * a2 + uvs[index + 2] * a3
 		return uv
 	else:
 		return Vector2()
@@ -102,8 +100,8 @@ func calculate_uv(event : InputEvent) -> Vector2:
 
 func _on_button_pressed() -> void:
 	DirAccess.remove_absolute(new_mask_path)
-	var image : Image = albedo_texture.get_image()
-	var image_texture : ImageTexture = ImageTexture.new()
+	var image: Image = albedo_texture.get_image()
+	var image_texture: ImageTexture = ImageTexture.new()
 	image_texture.set_image(image)
 	print(ResourceSaver.save(image_texture, new_mask_path))
 
@@ -118,7 +116,7 @@ func _on_green_button_pressed() -> void:
 
 func _on_blue_button_pressed() -> void:
 	color = Color.BLUE
-	
-	
+
+
 func _on_button_2_pressed() -> void:
 	print(get_tree().change_scene_to_file("res://shop.tscn"))
--- shop.gd
+++ shop.gd
@@ -1,35 +1,39 @@
 extends Node3D
 
-@export var mask_mesh : MeshInstance3D
+@export var mask_mesh: MeshInstance3D
 
-@onready var label : Label = $Control/Label
+@onready var label: Label = $Control/Label
+
 
 func _ready() -> void:
-	var curr_mask : ImageTexture = ResourceLoader.load("res://masks/curr_mask.res", "ImageTexture")
-	var base_mask : Image = load("res://masks/base_mask.png")
-	var curr_mask_size : Vector2i = curr_mask.get_size()
+	var curr_mask: ImageTexture = ResourceLoader.load("res://masks/curr_mask.res", "ImageTexture")
+	var base_mask: Image = load("res://masks/base_mask.res")
+	var curr_mask_size: Vector2i = curr_mask.get_size()
 	base_mask.resize(curr_mask_size.x, curr_mask_size.y)
 	_set_mask_info(curr_mask, base_mask)
 
 
 func _set_mask_info(mask_texture: ImageTexture, base_mask_image: Image) -> void:
-	var rgb : Vector3 = Vector3(0, 0, 0)
-	var mask_image : Image = mask_texture.get_image()
-	var size : Vector2i = mask_image.get_size()
+	var rgb: Vector3 = Vector3(0, 0, 0)
+	var mask_image: Image = mask_texture.get_image()
+	var size: Vector2i = mask_image.get_size()
+	var size_total: int = size.x * size.y
+	var file = FileAccess.open("res://pixel_info.txt", FileAccess.WRITE)
 	for x in range(size.x):
 		for y in range(size.y):
-			var pixel_color : Color = mask_image.get_pixel(x, y)
-			var base_pixel_color : Color = base_mask_image.get_pixel(x, y)
-			if pixel_color.is_equal_approx(base_pixel_color):
-				print("({2}, {3}) => {0} == {1}".format([str(pixel_color), str(base_pixel_color), x, y]))
+			var pixel_color: Color = mask_image.get_pixel(x, y)
+			var base_pixel_color: Color = base_mask_image.get_pixel(x, y)
+			if pixel_color.a < 1.0:
+				size_total -= 1
+			if pixel_color == base_pixel_color:
 				continue
-			if pixel_color.r > 0.8:
+			file.store_string("({0}, {1}) => {2} || {3}\n".format([x, y, pixel_color, base_pixel_color]))
+			if pixel_color.r > base_pixel_color.r:
 				rgb.x += 1
-			if pixel_color.g > 0.8:
+			elif pixel_color.g > base_pixel_color.g:
 				rgb.y += 1
-			if pixel_color.b > 0.8:
+			elif pixel_color.b > base_pixel_color.b:
 				rgb.z += 1
-	var size_total : int = size.x * size.y
 	label.text = "
 	Total: {3}
 	Red: {0}
@@ -38,8 +42,14 @@ func _set_mask_info(mask_texture: ImageTexture, base_mask_image: Image) -> void:
 	%Red: {4}
 	%Green: {5}
 	%Blue: {6}
-	".format([rgb.x, rgb.y, rgb.z, 
-	size_total, 
-	rgb.x / size_total * 100, 
-	rgb.y / size_total * 100, 
-	rgb.z / size_total * 100])
+	".format(
+		[
+			rgb.x,
+			rgb.y,
+			rgb.z,
+			size_total,
+			rgb.x / size_total * 100,
+			rgb.y / size_total * 100,
+			rgb.z / size_total * 100,
+		],
+	)
